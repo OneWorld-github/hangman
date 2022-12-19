3fd29d72959c69df3b19eaa4c85941fb60f83ff9
Timur <timur@spirable.com>
2022-12-19 18:56:05 +0000

    move milestone2 py file to root

--- hangman/milestone_2.py
+++
@@ -1,6 +0,0 @@
-#milestone_2.py
-
-word_list = ['blueberries', 'raspberries', 'kiwi', 'orange', 'pear']
-print(word_list)
-
-
---
+++ milestone_2.py
@@ -0,0 +1,18 @@
+#milestone_2.py
+#Task 1
+# %%
+word_list = ['blueberries', 'raspberries', 'kiwi', 'orange', 'pear']
+print(word_list)
+
+# %%
+#Task 2
+import random
+word = random.choice(word_list)
+print(word)
+# %%
+guess = input("Please enter a single letter")
+if len(guess) == 1 and guess.isalpha:
+    print("Good Guess")
+else:
+    print("oops that is not a valid input")
+
