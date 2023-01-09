bd24d9b0a47e637f3a2da94385618add2787e444
One World <timur@spirable.com>
2023-01-09 23:53:43 +0000

    attribute

--- milestone_4.py
+++ milestone_4.py
@@ -36,7 +36,7 @@ class Hangman:
             # Outside the for-loop, reduce the variable num_letters by 1.
             
             for index, letter in enumerate(self.word):
-                if letter == self.guess:
+                if letter == guess:
                     self.word_guessed[index] = guess
             self.num_letters -= 1
             print(f"The word is now {self.word_guessed}")
